f7c6fc722138aad761270b0d2e3f544a25e2bb8b
KhangDaSac <daophuckhang090@gmail.com>
2024-07-17 23:56:47 +0700

    Commit 17-07-2024, 23:56:43

---
+++ Assets/Scripts/Exchange Item/ExchangeInformationItem.cs
@@ -0,0 +1,19 @@
+using Microsoft.Unity.VisualStudio.Editor;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExchangeInformationItem : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
