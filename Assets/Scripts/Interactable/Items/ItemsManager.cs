9f024a1b6a33cc7f62c57507fe3aa7d55fb9cddb
khangdasac <145788969+khangdasac@users.noreply.github.com>
2026-08-19 12:42:50 +0700

    chỉnh sửa lỗi

--- Assets/Scripts/Interactable/Items/ItemsManager.cs
+++ Assets/Scripts/Interactable/Items/ItemsManager.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditorInternal.VersionControl;
 using UnityEngine;
 
 public abstract class ItemsManager : MonoBehaviour
